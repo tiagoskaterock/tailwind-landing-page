885b1f509b38b48d5f5b38ee71e8a2325647475f
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-18 04:30:11 -0300

    5. Introducción a Tailwind y sus clases de colores.

--- index.php
+++ index.php
@@ -8,16 +8,19 @@
 </head>
 <body>
 
-	<h1 class="text-2xl bg-red-800 text-white p-5 text-center w-2/3 mx-auto">Hola Tailwind!!!</h1>
+	<div class="text-center">
+		
+		<h1 class="text-center text-[steelblue] text-[100px]">Hola Tailwind!!!</h1>
 
-	<p class="w-2/3 mx-auto mt-5 p-5">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
+		<p class="text-zinc-600">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	</div>
 
 </body>
 </html>
