7a22b63373cdd8ea8053d28609e1ef9d75feb557
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-19 05:18:21 -0300

    27. PROYECTO LANDING PAGE - Creando la sección principal

---
+++ landing.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hola Tailwind!!!</title>
+	<link rel="stylesheet" type="text/css" href="src/css/estilos.css">
+</head>
+<body>	
+
+	<header class="bg-blue-500 min-h-[600px] h-screen">
+		<nav class="py-6 text-white grid justify-items-center items-center gp-6 wrapper">
+			<a href="#">
+				<h2 class="text-3xl font-extrabold">Landing.</h2>
+			</a>
+
+			<div class="space-x-4">
+				<a href="#" class="mr-4">Home</a>
+				<a href="#" class="mr-4">Habilidades</a>
+				<a href="#">Proyectos</a>
+			</div>
+		</nav>
+
+		<section class="wrapper">
+			<img src="girl.jpg" alt="Woman">
+
+			<div>
+				<h1>Yo soy Tiago, y será un placer ayudarte!</h1>
+			</div>
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+
+			<a href="#">Contáctame ahora</a>
+		</section>
+	</header>
+
+</body>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.php"],
+  content: ["./src/**/*.{html,js}", "./index.php", "./landing.php"],
   theme: {
     extend: {},
   },
